aee65300547095b4cfcde70e4b40e32e0a20b114
Deniz Soylular <dsoylular22@ku.edu.tr>
2024-03-16 10:55:53 +0300

    HomePage.dart oluşturuldu, bu sayfaya navigation yazıldı

--- lib/EntryPage.dart
+++ lib/EntryPage.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
+import 'HomePage.dart';
+
 class EntryPage extends StatefulWidget {
   const EntryPage({Key? key}) : super(key: key);
 
@@ -11,6 +13,23 @@ class EntryPage extends StatefulWidget {
 }
 
 class _EntryState extends State<EntryPage> {
+  late TextEditingController _emailController;
+  late TextEditingController _passwordController;
+
+  @override
+  void initState() {
+    super.initState();
+    _emailController = TextEditingController();
+    _passwordController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     final Size screenSize = MediaQuery.of(context).size;
@@ -52,7 +71,7 @@ class _EntryState extends State<EntryPage> {
                 ),
               ),
             ),
-            SizedBox(height: screenSize.height * (spacingRatio*2)),
+            SizedBox(height: screenSize.height * (spacingRatio * 2)),
             Container(
               width: screenSize.width * inputWidthRatio,
               height: screenSize.height * inputHeightRatio,
@@ -61,7 +80,8 @@ class _EntryState extends State<EntryPage> {
                 border: Border.all(color: Colors.grey),
                 borderRadius: BorderRadius.circular(screenSize.width * 0.1),
               ),
-              child: const TextField(
+              child: TextField(
+                controller: _emailController,
                 decoration: InputDecoration(
                   hintText: 'Email',
                   border: InputBorder.none,
@@ -79,7 +99,8 @@ class _EntryState extends State<EntryPage> {
                 border: Border.all(color: Colors.grey),
                 borderRadius: BorderRadius.circular(screenSize.width * 0.1),
               ),
-              child: const TextField(
+              child: TextField(
+                controller: _passwordController,
                 decoration: InputDecoration(
                   hintText: 'Password',
                   border: InputBorder.none,
@@ -91,13 +112,19 @@ class _EntryState extends State<EntryPage> {
             SizedBox(height: screenSize.height * (spacingRatio * 3)),
             ElevatedButton(
               onPressed: () {
-                //TODO: Ana ekrana geçiş
+                String email = _emailController.text;
+                String password = _passwordController.text;
+                //TODO: BU EMAIL VE PASSWORD DOĞRU MU CHECK YAPILMALI BUNA GÖRE ALT SATIRA GEÇECEK
+
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => HomePage(argument: email)),
+                );
               },
               style: ElevatedButton.styleFrom(
                 padding: EdgeInsets.symmetric(
                     horizontal: screenSize.width * 0.35,
-                    vertical: screenSize.width * 0.04
-                ),
+                    vertical: screenSize.width * 0.04),
                 backgroundColor: Colors.purple,
               ),
               child: const Text(
@@ -117,11 +144,10 @@ class _EntryState extends State<EntryPage> {
                 ),
                 children: <TextSpan>[
                   const TextSpan(
-                    text: "Don't have an account ? ",
-                    style: TextStyle(
-                      fontSize: 15,
-                    )
-                  ),
+                      text: "Don't have an account ? ",
+                      style: TextStyle(
+                        fontSize: 15,
+                      )),
                   TextSpan(
                     text: 'Register',
                     style: const TextStyle(
@@ -139,7 +165,6 @@ class _EntryState extends State<EntryPage> {
                 ],
               ),
             )
-
           ],
         ),
       ),
