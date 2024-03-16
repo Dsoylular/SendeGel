1e9dfafe0f5d84f534c7deccdb45382fc5258068
Deniz Soylular <dsoylular22@ku.edu.tr>
2024-03-16 16:35:20 +0300

    HomePage.dart third commit

--- lib/HubPageFiles/HomePage.dart
+++ lib/HubPageFiles/HomePage.dart
@@ -8,51 +8,98 @@ class HomeWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    List<String> myItems = [
+    List<String> attendedItems = [
       'Gezi 1',
-      'Gezi 2',
-      'Gezi 3',
-      'Gezi 4',
-      'Gezi 5',
-      'Gezi 6','Gezi 1',
-      'Gezi 2',
-      'Gezi 3',
-      'Gezi 4',
-      'Gezi 5',
-      'Gezi 6','Gezi 1',
       'Gezi 2',
       'Gezi 3',
       'Gezi 4',
       'Gezi 5',
       'Gezi 6',
     ];
+    List<String> organizedItems = [
+      'Organize 1',
+      'Organize 2',
+      'Organize 3',
+      'Organize 4',
+      'Organize 5',
+      'Organize 6',
+    ];
     return Scaffold(
       body: SingleChildScrollView(
         child: Column(
           children: [
-            SizedBox(height: 80),
-            Row(
-              children: [
-                SizedBox(width: 15),
-                Text(
-                  "Yaklaşan Gezilerim",
-                  style: TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.purple,
+            const SizedBox(height: 25),
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 15),
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.purple), // border color
+                borderRadius: BorderRadius.circular(20), // border radius
+              ),
+              child: const Padding(
+                padding: EdgeInsets.all(10.0),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    "Planlanmış Gezilerim",
+                    style: TextStyle(
+                      color: Colors.purple, // text color
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18,
+                    ),
                   ),
                 ),
-              ],
+              ),
             ),
-            SizedBox(height: 20),
-            Container(
-              height: 320,
-              child: Scrollbar(
+
+            const SizedBox(height: 10),
+            SizedBox(
+              height: 200,
+              child: attendedItems.isEmpty
+                  ? Center(child: Container(child: Text('Planlanmış geziniz yok, eklemek için tıklayın')))
+                  : Scrollbar(
                 thumbVisibility: true,
                 child: ListView(
                   shrinkWrap: true,
-                  itemExtent: 60,
-                  children: myItems
+                  itemExtent: 55,
+                  children: attendedItems
+                      .map((item) => Center(child: EventButton(buttonText: item)))
+                      .toList(),
+                ),
+              ),
+            ),
+            const SizedBox(height: 25),
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 15),
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.purple), // border color
+                borderRadius: BorderRadius.circular(20), // border radius
+              ),
+              child: const Padding(
+                padding: EdgeInsets.all(10.0),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    "Organize Ettiğim Geziler",
+                    style: TextStyle(
+                      color: Colors.purple, // text color
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 10,),
+            SizedBox(
+              height: 200,
+              child: attendedItems.isEmpty
+                  ? Center(child: Container(child: Text('Planlanmış geziniz yok, eklemek için tıklayın')))
+                  : Scrollbar(
+                thumbVisibility: true,
+                child: ListView(
+                  shrinkWrap: true,
+                  itemExtent: 55,
+                  children: organizedItems
                       .map((item) => Center(child: EventButton(buttonText: item)))
                       .toList(),
                 ),
