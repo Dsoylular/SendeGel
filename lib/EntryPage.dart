7f80c25876c244d473d3187388b920dccce71109
Deniz Soylular <dsoylular22@ku.edu.tr>
2024-03-16 03:44:35 +0300

    login Page initial design is finished

--- lib/EntryPage.dart
+++ lib/EntryPage.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
 
 class EntryPage extends StatefulWidget {
@@ -18,23 +17,121 @@ class _EntryState extends State<EntryPage> {
       body: Column(
         children: [
           Container(
-            color: Colors.purple,
+            width: double.infinity,
+            height: 350,
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [
+                  Colors.deepPurple,
+                  Colors.purple,
+                ],
+              ),
+            ),
             child: const SizedBox(
               width: double.infinity,
               height: 350,
               child: Center(
                 child: Text(
                   "Sende Gel",
-                      style: TextStyle(
-                        fontSize: 40.0,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white,
-                        fontStyle: FontStyle.italic,
-                      ),
+                  style: TextStyle(
+                    fontSize: 40.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                    fontStyle: FontStyle.italic,
+                  ),
                 ),
               ),
             ),
+          ),
+          const SizedBox(height: 40),
+
+          Center(
+            child: Container(
+              width: 300,
+              height: 60,
+              padding: EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.grey),
+                borderRadius: BorderRadius.circular(25),
+              ),
+              child: const TextField(
+                decoration: InputDecoration(
+                  hintText: 'Email',
+                  border: InputBorder.none,
+                  contentPadding: EdgeInsets.zero,
+                  prefixIcon: Icon(Icons.email),
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(height: 20),
+
+          Center(
+            child: Container(
+              width: 300,
+              height: 60,
+              padding: EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.grey),
+                borderRadius: BorderRadius.circular(25),
+              ),
+              child: const TextField(
+                decoration: InputDecoration(
+                  hintText: 'Password',
+                  border: InputBorder.none,
+                  contentPadding: EdgeInsets.zero,
+                  prefixIcon: Icon(Icons.key),
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(height: 100),
+
+          Center(
+            child: ElevatedButton(
+                onPressed: () {
+                //TODO:  HERE SHOULD BE FILLED!!!!!
+                },
+                style: ElevatedButton.styleFrom(
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 130, vertical: 15
+                  ),
+                  backgroundColor: Colors.purple,
+                ),
+                child: const Text(
+                    "LOGIN",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+            ),
+          ),
+          const SizedBox(height: 90),
+
+          RichText(
+            text: const TextSpan(
+              style: TextStyle(
+                fontSize: 16.0,
+                color: Colors.black,
+              ),
+              children: <TextSpan>[
+                TextSpan(
+                  text: "Don't have an account ? ",
+                ),
+                TextSpan(
+                  text: 'Register',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.purple,
+                  ),
+                ),
+              ],
+            ),
           )
+
         ],
       ),
     );
