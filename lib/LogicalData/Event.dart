46c2b7ffa0386700fdf902a692ec6dc0be766d25
MevlutBasaran <mevlutbasaran01@gmail.com>
2024-03-18 14:04:58 +0300

    Test

--- lib/LogicalData/Event.dart
+++ lib/LogicalData/Event.dart
@@ -1,5 +1,4 @@
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-
 import 'Member.dart';
 
 class Event{
--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: EventWidget(members: members, events: events, currentUser: currentUser),
     );
