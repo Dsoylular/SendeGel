8eaf256bcc93814f25b76ab857b3c6d140a9052f
Deniz Soylular <dsoylular22@ku.edu.tr>
2024-06-13 15:31:02 +0300

    Logo and name change

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -12,7 +12,7 @@ import 'package:appjamteam39/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(MyApp(currentUser: globalMemberList[14], members: globalMemberList, events: globalEventList));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
